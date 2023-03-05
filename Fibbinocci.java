991db69ee539e1be92ad2825409fced86ca674a1
naga612 <127010815+naga612@users.noreply.github.com>
2023-03-05 13:19:16 +0530

    Add files via upload
    Week1Day1

---
+++ Fibbinocci.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Fibbinocci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int firstnum = 0;
+int secondnum = 1;
+int sum = 0;
+for (int i = 1; i <=11; i++) {
+System.out.println(firstnum+ " ");
+sum = firstnum+secondnum;
+firstnum=secondnum;
+secondnum=sum;
+}
+{
+	
+}
+	}
+
+}
---
+++ Variablepersonaldetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Variablepersonaldetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int luckynum = 6;
+	Boolean yesorno = true;
+	char initial     = 'S';
+	String name = "Naga";
+	long mob = 123456789l;
+	
+	System.out.println(name);
+	System.out.println(initial);
+	System.out.println(luckynum);
+	System.out.println(yesorno);
+	System.out.println(mob);
+
+	}
+
+}
