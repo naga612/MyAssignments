94a6315f350a9a05ca37b7c91954e1d22f6f046b
naga612 <127010815+naga612@users.noreply.github.com>
2023-03-06 23:53:38 +0530

    Add files via upload

---
+++ IsPrime.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		int num = 8;
+	for (int i = 2; i<=num; i++) {	
+	if (num%i!=0) {
+System.out.println("The number is Prime");
+	break;
+	} 
+	else {
+System.out.println("The number is NonPrime");
+break;
+	}	
+	}
+	
+	}
+
+}
